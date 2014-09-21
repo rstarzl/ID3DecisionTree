93d50c5ede239e91eec43e6f124ecf1dbb0ca807
Ravi Starzl <rstarzl@cs.cmu.edu>
2014-09-21 10:26:08 -0400

    Addeed command line argument check

---
+++ ID3DecisionTree/src/Grow/ID3DecisionTree.java
@@ -0,0 +1,19 @@
+package Grow;
+
+public class ID3DecisionTree {
+
+	public static void main(String[] args) {
+		
+		if(args.length == 0) {
+			System.out.println("Usage: program [inputFile]");
+			try {
+				throw new Exception("Insufficient arguments");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		String inputFile = args[0];
+	}
+
+}
