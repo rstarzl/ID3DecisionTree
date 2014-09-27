04467141e4e53ff45e02ee991206d8f67f16e28e
rstarzl <rstarzl@cs.cmu.edu>
2014-09-27 15:32:25 -0400

    Added leaf and node classes for the decision tree itself.

---
+++ ID3DecisionTree/src/structure/Leaf.java
@@ -0,0 +1,51 @@
+package structure;
+import java.util.ArrayList;
+
+public class Leaf {
+
+	private ArrayList<Integer> instanceIndices = new ArrayList<Integer>();
+	private Integer parent;
+	private Integer leafClass;
+	private Boolean growing = true;
+
+	public ArrayList<Integer> getInstanceIndices() {
+		return instanceIndices;
+	}
+
+	public void setInstanceIndices(ArrayList<Integer> instanceIndices) {
+		this.instanceIndices = instanceIndices;
+	}
+	
+	public void addToInstanceIndices(Integer instanceID) {
+		this.instanceIndices.add(instanceID);
+	}
+	
+	public void removeFromInstanceIndices(Integer instanceID) {
+		this.instanceIndices.remove(instanceID);
+	}
+
+	public Integer getParent() {
+		return parent;
+	}
+
+	public void setParent(Integer parent) {
+		this.parent = parent;
+	}
+
+	public Integer getLeafClass() {
+		return leafClass;
+	}
+
+	public void setLeafClass(Integer leafClass) {
+		this.leafClass = leafClass;
+	}
+
+	public Boolean getGrowing() {
+		return growing;
+	}
+
+	public void setGrowing(Boolean growing) {
+		this.growing = growing;
+	}
+	
+}
---
+++ ID3DecisionTree/src/structure/Node.java
@@ -0,0 +1,35 @@
+package structure;
+
+public class Node {
+
+	private Double threshold;
+	private Integer child0;
+	private Integer child1;
+	private Integer parent;
+	
+	public Double getThreshold() {
+		return threshold;
+	}
+	public void setThreshold(Double threshold) {
+		this.threshold = threshold;
+	}
+	public Integer getChild0() {
+		return child0;
+	}
+	public void setChild0(Integer child0) {
+		this.child0 = child0;
+	}
+	public Integer getChild1() {
+		return child1;
+	}
+	public void setChild1(Integer child1) {
+		this.child1 = child1;
+	}
+	public Integer getParent() {
+		return parent;
+	}
+	public void setParent(Integer parent) {
+		this.parent = parent;
+	}
+	
+}
